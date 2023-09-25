a0bc2773d6700de06035f6ba907de9dfd4c8a535
Febryan Ramadhan <febryanramadhan@gmail.com>
2023-09-25 10:01:18 +0700

    added host dedicated description, added flavors list, etc

--- get-data-aio.sh
+++ get-data-aio.sh
@@ -62,6 +62,24 @@ mv temp_combined.csv aio.csv
 
 openstack hypervisor list --long -f value > allocation.txt
 
-scp aio.csv allocation.txt ubuntu@${instance_server}
+# openstack compute service list --long -f value > compute_service.txt
+
+openstack flavor list --all --long -f value | sed 's/ /|/g' | sed 's/,|/, /g' > flavors.csv
+sed -i '1iID|Name|RAM|Disk|Ephemeral|VCPUs|Is Public|Swap|RXTX Factor|Properties' flavors.csv
+
+# file_hosts="hosts.txt"
+# for host in $(cat "$file_hosts"); do
+#     ssh_result=$(ssh "$host" "sudo cat /etc/nova/nova.conf")
+  
+#     cpu_ratio=$(echo "$ssh_result" | grep -oP "cpu_allocation_ratio = \K\d+(\.\d+)?")
+
+#     ram_ratio=$(echo "$ssh_result" | grep -oP "ram_allocation_ratio = \K\d+(\.\d+)?")
+
+#     cpu_ratio=$(echo "$cpu_ratio" | sed 's/\.0$//')
+#     #ram_ratio=$(echo "$ram_ratio" | sed 's/\.0$//')
+#     echo "$host, $cpu_ratio, $ram_ratio"
+# done > ratio.txt
+
+scp aio.csv allocation.txt flavors.csv ubuntu@${instance_server}
 
 # rm -f temp_*
